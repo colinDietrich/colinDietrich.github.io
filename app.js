807dacb659312350b4fa8091d9d7c97881353806
colinDietrich <44623378+colinDietrich@users.noreply.github.com>
2024-02-17 17:56:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('.nav-link');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            navLinks.forEach(node => node.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-});
